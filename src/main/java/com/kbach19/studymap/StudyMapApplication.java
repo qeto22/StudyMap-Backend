ca32e831429e2a288c2dd998628eb917f7445a6c
Ketevan Bachalashvili <kbach19@freeuni.edu.ge>
2023-08-05 03:04:22 +0400

    Initial version of the Spring StudyMap backend

---
+++ src/main/java/com/kbach19/studymap/StudyMapApplication.java
@@ -0,0 +1,13 @@
+package com.kbach19.studymap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyMapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kbach19/studymap/StudyMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kbach19.studymap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
