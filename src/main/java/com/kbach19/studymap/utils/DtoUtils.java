279ad05e5c68cd19cde55622ba9ccc6c952eeec0
Ketevan Bachalashvili <kbach19@freeuni.edu.ge>
2023-09-02 02:32:00 +0400

    Backend changes for posting Reviews

--- src/main/java/com/kbach19/studymap/utils/DtoUtils.java
+++ src/main/java/com/kbach19/studymap/utils/DtoUtils.java
@@ -2,6 +2,7 @@ package com.kbach19.studymap.utils;
 
 import com.kbach19.studymap.api.dto.Author;
 import com.kbach19.studymap.api.dto.PostReviewRequest;
+import com.kbach19.studymap.api.dto.ReviewResponse;
 import com.kbach19.studymap.model.Review;
 import com.kbach19.studymap.model.SystemUser;
 
@@ -18,9 +19,17 @@ public class DtoUtils {
 
     public static Review getReview(PostReviewRequest request) {
         return Review.builder()
-                .comment(request.getComment())
+                .comment(request.getReviewText())
                 .rating(request.getRating())
                 .author(AuthUtils.getAuthenticatedUser())
+                .build();
+    }
+
+    public static ReviewResponse getPostReviewResponse(Review postedReview) {
+        return ReviewResponse.builder()
+                .reviewText(postedReview.getComment())
+                .rating(postedReview.getRating())
+                .author(toDTO(postedReview.getAuthor()))
                 .build();
     }
 }
